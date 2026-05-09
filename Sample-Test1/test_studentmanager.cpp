8fb41b38d2990a0dd3d1d90af4db31e43c8b25fe
Zhang <Zhang747326@outlook.com>
2026-05-10 00:06:47 +0800

    rty

--- Sample-Test1/test_studentmanager.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include "pch.h"
-#include <iostream>
-#include "../Heyiwei2/StudentManager.h"
-
-TEST(studentManager_test, test1) {
-	StudentManager studentManager;
-	std::cout << studentManager.outputInfo();
-}
