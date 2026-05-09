36cb288e808d38d9785eddcf3368a5b9f0f20c62
Zhang <Zhang747326@outlook.com>
2026-05-09 20:56:21 +0800

    数据模型

--- Heyiwei2/Model.cpp
+++ Heyiwei2/Model.cpp
@@ -1,15 +1,41 @@
 #include "pch.h"
 #include <iostream>
 
-class Result {
-public:
+struct Result {
 	bool success;
 	std::string message;
 };
 
-class WaterRecord {
-public:
+struct WaterRecord {
 	int year;
 	int month;
+	double usage;
+	double cost;
+};
 
+struct Student {
+ 	std::string name;
+	std::string studentId;
+};
+
+struct Dorm {
+	std::vector<Student> students;
+	std::vector<WaterRecord> records;
+	DormInfo info;
+};
+
+struct DormInfo {
+	Region region;
+	int buildingNumber; // 楼号
+	int floor; // 层数
+	int roomNumber; // 门牌号
+	std::string toString() {
+		std::string regionStr = (region == Region::East) ? "东" : "西";
+		return regionStr + "-" + std::to_string(buildingNumber) + "-" + std::to_string(floor) + "-" + std::to_string(roomNumber);
+	}
+};
+
+enum Region {
+	East,
+	West,
 };
