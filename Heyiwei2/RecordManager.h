8ad2c636b656c217427307921f582b83b5dcfcf4
godme6661 <2963355030@qq.com>
2026-05-11 12:53:15 +0800

    Revert "111"
    This reverts commit ae90ed28dd83a39ad748b16df4bb8e9214514f33.

--- Heyiwei2/RecordManager.h
+++ Heyiwei2/RecordManager.h
@@ -1,18 +1,5 @@
 ﻿#pragma once
-#include "Models.h"
-
 class RecordManager
 {
-    private:
-    std::vector<Models::WaterRecord>& waterRecords;
-
-public:
-    RecordManager(std::vector<Models::WaterRecord>& records);
-
-    Models::Result addWaterRecord(const Models::WaterRecord& record);
-    void removeWaterRecord(int year, int month);
-    void updateWaterRecord(int year, int month, double usage);
-    std::vector<Models::WaterRecord> queryWaterRecord(int year, int month);
-    Models::WaterRecord* querySpecificWaterRecord(const std::string& id, int year, int month);
 };
 
