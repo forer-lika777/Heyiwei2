7ee6bd9bfc23588e12b1157974d66b75075f6eaa
godme6661 <2963355030@qq.com>
2026-05-11 12:14:48 +0800

    111

--- Heyiwei2/RecordManager.h
+++ Heyiwei2/RecordManager.h
@@ -3,6 +3,16 @@
 
 class RecordManager
 {
-	Models::WaterRecord
+    private:
+    std::vector<Models::WaterRecord>& waterRecords;
+
+public:
+    RecordManager(std::vector<Models::WaterRecord>& records);
+
+    Models::Result addWaterRecord(const Models::WaterRecord& record);
+    void removeWaterRecord(int year, int month);
+    void updateWaterRecord(int year, int month, double usage);
+    std::vector<Models::WaterRecord> queryWaterRecord(int year, int month);
+    Models::WaterRecord* querySpecificWaterRecord(const std::string& id, int year, int month);
 };
 
