c57b945853c87162fdf1392b5acb9f3c7e27b55e
bamboo_yongqi <1832154726@qq.com>
2026-05-13 23:18:42 +0800

    123

--- Heyiwei2/Utils.cpp
+++ Heyiwei2/Utils.cpp
@@ -1,2 +1,6 @@
 #include "pch.h"
 #include "Utils.h"
+double Utils::calculateTotalWaterFee(double waterUsage, double unitPrice)
+{
+	return waterUsage * unitPrice;
+}
--- Heyiwei2/Utils.h
+++ Heyiwei2/Utils.h
@@ -1,5 +1,8 @@
 #pragma once
 class Utils
 {
+	//计算总水费
+	static double calculateTotalWaterFee();
+
 };
 
