b867006b05c41ec81171e2820307e8791fdda11f
bamboo_yongqi <1832154726@qq.com>
2026-05-14 01:09:55 +0800

    874

--- Heyiwei2/Utils.h
+++ Heyiwei2/Utils.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 class Utils
 {
+public:
 	//计算总水费
-	static double calculateTotalWaterFee();
+	static double calculateTotalWaterFee(Heyiwei2::Models::WaterRecord const& record);
+	//检验学生ID是否合法
+	static bool validateStudentId(Heyiwei2::Models::Student const& record);
+	//检测水费记录是否合法
+	static bool validateWaterRecord(Heyiwei2::Models::WaterRecord const& record);
+	//排序水费记录
+	static void sortWaterRecords(vector<Heyiwei2::Models::WaterRecord>& records);
 
 };
 
